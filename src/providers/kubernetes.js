1b41973cea0a4765c1919cf38acdc4695548d6f8
Casey Foster <c@sey.me>
2016-10-06 10:18:46 -0500

    Add forceful pod termination after grace period, 0.1.0

--- src/providers/kubernetes.js
+++ src/providers/kubernetes.js
@@ -42,14 +42,33 @@ exports.start = (client,
       })
     ).then(({metadata: {name}}) => name);
 
-const stop = exports.stop = (client, {namespace, id}) =>
-  client.delete(`namespaces/${namespace}/pods/${id}`);
+const stop = exports.stop = (client, {namespace, id, gracePeriodSeconds}) =>
+  client.delete(`namespaces/${namespace}/pods/${id}`, {gracePeriodSeconds});
 
 const remove = exports.remove = (client, {namespace, id}) =>
   client.get(`namespaces/${namespace}/pods/${id}`).then(
-    ({metadata: {deletionTimestamp}}) =>
-      (deletionTimestamp ? wait(TEN_SECONDS) : stop(client, {namespace, id}))
-        .then(() => remove(client, {namespace, id})),
+    ({
+      metadata: {deletionTimestamp},
+      spec: {terminationGracePeriodSeconds}
+    }) => {
+      let promise = Promise.resolve();
+
+      // If the pod hasn't been scheduled to terminate yet, do that first.
+      if (!deletionTimestamp) promise = stop(client, {namespace, id});
+      else {
+        const delta = (_.now() - new Date(deletionTimestamp)) / 1000;
+
+        // If the pod is terminating check to see if it is still within its
+        // grace period. If so, wait and try again.
+        if (delta < terminationGracePeriodSeconds) promise = wait(TEN_SECONDS);
+
+        // Otherwise forcefully kill the pod as it may be indefinitely stuck in
+        // the terminating state.
+        else promise = stop(client, {namespace, id, gracePeriodSeconds: 0});
+      }
+
+      return promise.then(() => remove(client, {namespace, id}));
+    },
     er => {
       try { assert.equal(JSON.parse(er).code, 404); } catch (__) { throw er; }
     }
